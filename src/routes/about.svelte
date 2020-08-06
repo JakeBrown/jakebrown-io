d7773ef5427a7294e8636ade84b9b70a49b30566
Jake Brown <jake@eyespacelenses.com>
2020-08-06 17:53:05 +0930

    Updated about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -34,8 +34,8 @@
 
   <p>In 2012 I completed Honours in High Performance Computational Phyiscs at the University of Adelaide. It's billed as <i>solving difficult Physics problems with supercomputers</i>. But for me, it was about trying to understand the world through mathematics and abstraction, and <i>finding programmatic solutions to real world problems</i>.</p>
 
-  <p>At EyeSpace™, which I co-founded in 2010, we produce software used by eye doctors around the world for designing, simulating, and manufacturing custom contact lenses.
-  Our job is to enable and assist doctors with treating difficult eye conditions, often with life-changing results.</p>
+  <p>At EyeSpace™ which I co-founded in 2010, we produce software used by eyecare practitioners and medical device manufacturers around the world for designing, simulating, and manufacturing custom contact lenses.
+  Our goal is to make it easier for eyecare practitioners to improve the lives of their patients. You can read more <a href='https://www.eyespacelenses.com'>here</a>.
 
   <p>I have a deep interest in software architecture and modern development practices.</p>
   <p>Outside of this, I enjoy hiking in the great outdoors, freediving, rock climbing, and powerlifting.</p>
