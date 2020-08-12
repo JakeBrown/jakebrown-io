c0783381a4328cd8bf64b2e4c828f51e3a8c8908
Jake Brown <jake@eyespacelenses.com>
2020-08-13 09:00:51 +0930

    Added select

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -1,3 +1,48 @@
+<style>
+  /* Style inputs with type="text", select elements and textareas */
+  input[type='text'],
+  input[type='email'],
+  select,
+  textarea {
+    width: 100%; /* Full width */
+    padding: 12px; /* Some padding */
+    border: 1px solid #ccc; /* Gray border */
+    border-radius: 4px; /* Rounded borders */
+    box-sizing: border-box; /* Make sure that padding and width stays in place */
+    margin-top: 6px; /* Add a top margin */
+    margin-bottom: 16px; /* Bottom margin */
+    resize: vertical; /* Allow the user to vertically resize the textarea (not horizontally) */
+    font-size: 1rem;
+  }
+
+  select {
+    height: 2rem;
+  }
+
+  /* Style the submit button with a specific background color etc */
+  button {
+    background-color: #4caf50;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 1rem;
+  }
+
+  /* When moving the mouse over the submit button, add a darker green color */
+  button:hover {
+    background-color: #45a049;
+  }
+
+  /* Add a background color and some padding around the form */
+  .container {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+  }
+</style>
+
 <script>
   let name = ''
   let email = ''
@@ -10,18 +55,30 @@
       )
       .join('&')
   }
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
     console.log('submitting')
     let payload = { 'form-name': 'contact', name, email, message }
     console.log(payload)
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode(payload),
-    })
-      .then(() => alert('Success!'))
-      .catch((error) => alert(error))
+    try {
+      let response = await fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encode(payload),
+      })
+      console.log(response)
+      console.log(response.url)
+      if (response.url.includes('index.html')) {
+        alert('Intercepted by index.html. Not available locally.')
+        return
+      }
+      alert('Thanks for your message!')
+      name = ''
+      email = ''
+      message = ''
+    } catch (err) {
+      alert(err)
+    }
   }
 </script>
 
@@ -30,31 +87,27 @@
 </svelte:head>
 <div class="container">
   <h1>Contact</h1>
+  <p>If you need to get in contact with me you can fill out this form:</p>
   <form class="contactForm">
-    <div class="form-group">
-      <input
-        type="text"
-        bind:value={name}
-        class="form-control"
-        id="name"
-        placeholder="Your Name" />
-    </div>
-    <div class="form-group">
-      <input
-        type="email"
-        class="form-control"
-        bind:value={email}
-        id="email"
-        placeholder="Your Email" />
-    </div>
-    <div class="form-group">
-      <textarea
-        class="form-control"
-        name="message"
-        bind:value={message}
-        rows="5"
-        placeholder="Message" />
-    </div>
+    <input type="text" bind:value={name} id="name" placeholder="Your Name" />
+    <input
+      type="email"
+      bind:value={email}
+      id="email"
+      placeholder="Your Email" />
+    <label for="country">What is this regarding?</label>
+    <select name="reason[]">
+      <option value="0">Select one...</option>
+      <option value="EyeSpace">EyeSpace&trade;</option>
+      <option value="Other Existing Project">Other Existing Project</option>
+      <option value="New Project">New Project</option>
+      <option value="Something else">Something else</option>
+    </select>
+    <textarea
+      name="message"
+      bind:value={message}
+      rows="5"
+      placeholder="Message" />
     <button on:click={handleSubmit} class="btn btn-send">Send</button>
   </form>
 </div>
