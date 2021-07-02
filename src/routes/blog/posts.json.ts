2d41a28c47c91a2700bce776b1f4b9a3fe11107a
Jake Brown <jake@eyespacelenses.com>
2021-07-02 21:46:41 +0930

    Fixed static url

--- src/routes/blog/posts.json.ts
+++ src/routes/blog/posts.json.ts
@@ -2,9 +2,7 @@ export async function get() {
     let posts = import.meta.glob('./posts/*.svx');
     console.log('Running request route');
     let body = [];
-    console.log(posts);
     for (const path in posts) {
-        console.log(path);
         let post = await posts[path]();
         body.push({
             metadata: post.metadata,
